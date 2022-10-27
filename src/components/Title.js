bff4d27b85f5ae219fa551ad8aac3788765a95e6
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-10-27 16:18:56 +0900

    제목 수정

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react';
 
-const Title=() =>(
-            <div>
-                <h1 className="title-container__title"> Check the Weather!</h1>
-                <p className="title-container__subtitle"> Find out the weather today...</p>
-            </div>
-        );
-
- 
+const Title = () => (
+  <div>
+    <h1 className="title-container__title"> Check the Weather!zzzzzs</h1>
+    <p className="title-container__subtitle"> Find out the weather today...</p>
+  </div>
+);
 
 export default Title;
